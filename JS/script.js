fc70abd2408a4ab784039064ffc8d5f0e574bb59
4k4shi <4k4shisj@gmail.com>
2020-03-11 21:41:57 -0300

    segunda

--- JS/script.js
+++ JS/script.js
@@ -17,3 +17,25 @@ let revelar = function (){
 }
 
 olho.addEventListener("click",revelar)
+
+//2ª QUESTÃO
+let preencherTUDO = function(e){
+		let verificar = 1;
+		let todosCampos = document.querySelector("form").querySelectorAll("input");
+		for(let j = 0;j < todosCampos.length;j++){
+			if((todosCampos[j].type == "text" || todosCampos[j].type == "password" || todosCampos[j].type == "number") && todosCampos[j].value == ""){
+				todosCampos[j].style = 'border: dashed 1px red;border-radius: 6px;';
+				e.cancelable;
+				e.preventDefault();	
+				verificar = 0;
+			}else{
+				todosCampos[j].style = 'border: solid 1px transparent';
+			}
+		}
+		if(verificar == 1){
+			alert("Adicionamento feito com sucesso!!!");
+		}else{
+			alert("Algum campo em branco")
+		}
+}
+document.getElementsByClassName("sumbit")[0].addEventListener("click",preencherTUDO);
