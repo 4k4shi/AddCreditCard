6e828223c26bfbb791b65e0b624f5fde7723d19f
4k4shi <4k4shisj@gmail.com>
2020-03-11 21:53:33 -0300

    segunda

--- JS/script.js
+++ JS/script.js
@@ -18,7 +18,7 @@ let revelar = function (){
 
 olho.addEventListener("click",revelar)
 
-//2ª QUESTÃO
+//2ª e 3ª QUESTÃO
 let preencherTUDO = function(e){
 		let verificar = 1;
 		let todosCampos = document.querySelector("form").querySelectorAll("input");
@@ -34,8 +34,34 @@ let preencherTUDO = function(e){
 		}
 		if(verificar == 1){
 			alert("Adicionamento feito com sucesso!!!");
+			if(e.cancelable){
+				let final,nome,dia,mes;
+
+				final = document.createTextNode(todosCampos[3].value);
+				nome = document.createTextNode(todosCampos[5].value);
+				dia = document.createTextNode(todosCampos[6].value);
+				mes = document.createTextNode(todosCampos[7].value);
+				e.preventDefault();
+
+			}
 		}else{
 			alert("Algum campo em branco")
 		}
 }
 document.getElementsByClassName("sumbit")[0].addEventListener("click",preencherTUDO);
+
+/*
+ <div class="item">
+    <div class="logo">
+        <img src="https://brand.mastercard.com/content/dam/mccom/brandcenter/thumbnails/mastercard_circles_92px_2x.png"
+             alt=""/>
+    </div>
+    <p>
+        <strong>MasterCard final 1234</strong><br>
+        <small>Fulando da Silva</small><br/>
+        <small>Vencimento <span>10/10</span></small><br/>
+    </p>
+
+    <div class="close"></div>
+</div>
+*/
